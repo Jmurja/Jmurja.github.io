c921245c8e0251827727cfd966474969bdd4d280
murja <jmurja.filho@gmail.com>
2024-09-27 15:03:03 -0300

    improvement ui, and att text about me

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Menu mobile toggle
 const menuBtn = document.getElementById('menu-btn');
 const menu = document.getElementById('menu');
 const menuOpenIcon = document.getElementById('menu-open-icon');
@@ -10,23 +9,20 @@ menuBtn.addEventListener('click', () => {
   menuCloseIcon.classList.toggle('hidden');
 });
 
-// Scroll suave ao clicar nos links da navbar
-document.querySelectorAll('nav a').forEach(anchor => {
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
     menu.classList.add('hidden'); // Fecha o menu ao clicar
     menuOpenIcon.classList.remove('hidden');
     menuCloseIcon.classList.add('hidden');
-    const targetId = this.getAttribute('href').substring(1);
-    const targetElement = document.getElementById(targetId);
-    targetElement.scrollIntoView({ behavior: 'smooth' });
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
   });
 });
 
-// Botão de voltar ao topo
 const topButton = document.getElementById('top-button');
 
-// Exibir o botão ao rolar
 window.addEventListener('scroll', () => {
   if (window.scrollY > 300) {
     topButton.classList.remove('hidden');
@@ -35,14 +31,9 @@ window.addEventListener('scroll', () => {
   }
 });
 
-// Voltar ao topo ao clicar no botão
 topButton.addEventListener('click', () => {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 });
-
-// Alternar tema (dark/light) com preferência do usuário
-const themeToggle = document.getElementById('theme-toggle');
-const htmlElement = document.documentElement;
 
 window.addEventListener('load', () => {
   document.getElementById('loader').style.display = 'none';
